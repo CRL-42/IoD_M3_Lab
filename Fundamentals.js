cbb326ee8e1f75a9692e5433e6ef993c8fa023b3
CRL-42 <lodgeca42@gmail.com>
2026-08-23 12:20:22 +1200

    JS fundamentals lab exercise complete.

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,5 +1,5 @@
 // Q1)
-console.log("--Start Q1--");
+console.log("--Start Q1--\n");
 
 "" + 1 + 0; // = 10
 console.log("" + 1 + 0); //correct
@@ -36,9 +36,9 @@ console.log(undefined === null); //correct
 " \t \n" - 2; // = undefined
 console.log(" \t \n" - 2); //incorrect = -2
 
-console.log("--END Q1--");
+console.log("\n--END Q1--");
 // Q2)
-console.log("--Start Q2--");
+console.log("--Start Q2--\n");
 
 let three = "3";
 let four = "4";
@@ -65,9 +65,9 @@ console.log(lessThan2);
 console.log(30 < 4);
 // the incorect answeres are because the number values stored in the variable are strings, if set as numbers e.g. 3, 4, 30, the math would be correct.
 
-console.log("--END Q2--");
+console.log("\n--END Q2--");
 // Q3)
-console.log("--Start Q3--");
+console.log("--Start Q3--\n");
 
 if (0) console.log("#1 zero is true"); //Print fails. 0 returns false as has zero value.
 if ("0") console.log("#2 zero is true"); //Print succeeds. String "0" returns true as the string has a value.
@@ -76,9 +76,9 @@ if (-1) console.log("negative is true"); //Print succeeds. -1 returns true as ha
 if (1) console.log("positive is true"); //Prints succeeds. 1 returns true as has a value of 1.
 // The console.log comand runs when the if statment is true.
 
-console.log("--END Q3--");
+console.log("\n--END Q3--");
 // Q4)
-console.log("--Start Q4--");
+console.log("--Start Q4--\n");
 
 let a = 2,
   b = 3;
@@ -90,9 +90,9 @@ a + b < 10 ? (result += "less than 10") : (result += "greater than 10");
 console.log(result);
 // the += concatenates the values on the left and right together.
 
-console.log("--END Q4--");
+console.log("\n--END Q4--");
 // Q5)
-console.log("--Start Q5--");
+console.log("--Start Q5--\n");
 
 function getGreeting(name) {
   return "Hello " + name + "!";
@@ -109,9 +109,9 @@ const greetingArrow = (name) => {
 };
 console.log(greetingArrow("Sayan"));
 
-console.log("--END Q5--");
+console.log("\n--END Q5--");
 // Q6)
-console.log("--Start Q6--");
+console.log("--Start Q6--\n");
 
 const westley = {
   name: "Westley",
@@ -122,17 +122,158 @@ const rugen = {
   numFingers: 6,
 };
 const inigo = {
-  firstName: "Inigo", lastName: "Montoya",
+  firstName: "Inigo",
+  lastName: "Montoya",
   greeting(person) {
-    let greeting = `Hello ${person.name}, my name is ${this.firstName + ' ' + this.lastName}. `;
+    let greeting = `Hello ${person.name}, my name is ${this.firstName + " " + this.lastName}. `;
     console.log(greeting + this.getCatchPhrase(person));
   },
-  getCatchPhrase(person) {
-    if numFingers == 6:
-    return "You killed my father. Prepare to die"
-    else
-    return "Nice to meet you.";
-  },
+  getCatchPhrase: (person) =>
+    person.numFingers === 6
+      ? "You killed my father. Prepare to die."
+      : "Nice to meet you.",
 };
 inigo.greeting(westley);
 inigo.greeting(rugen);
+
+console.log("\n--END Q6--");
+// Q7)
+console.log("--Start Q7--\n");
+
+const basketballGame = {
+  score: 0,
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  fouls: 0,
+  foul() {
+    this.fouls++;
+    return this;
+  },
+    halfTime() {
+    console.log("Halftime score is " + this.score);
+    console.log("Halftime fouls " + this.fouls);
+    return this;
+  },
+  fullTime() {
+    console.log("Final score is " + this.score);
+    console.log("Final fouls " + this.fouls)
+  }
+};
+//modify each of the above object methods to enable function chaining as below:
+console.log('-Basketball Game 1')
+const basketballGame1 = {...basketballGame};
+basketballGame1.basket().freeThrow().foul().freeThrow().basket().threePointer().halfTime().freeThrow().foul().basket().fullTime();
+console.log('-Basketball Game 2')
+const basketballGame2 = {...basketballGame};
+basketballGame2.threePointer().basket().foul().freeThrow().halfTime().threePointer().threePointer().foul().basket().freeThrow().foul().freeThrow().foul().freeThrow().fullTime();
+
+console.log("\n--END Q7--");
+// Q8)
+console.log("--Start Q8--\n");
+
+const sydney = {
+name: 'Sydney',
+population: 5_121_000,
+state: 'NSW',
+founded: '26 January 1788',
+timezone: 'Australia/Sydney'
+}
+
+getCity(sydney)
+
+function getCity(city) {
+  for (let key in city) {
+    console.log(key + ': ' + city[key])
+  }
+}
+
+const auckland = {
+name: 'Auckland',
+population: 1_816_000,
+Region: 'AKL',
+founded: '18 September 1840',
+timezone: 'New Zealand'
+}
+
+getCity(auckland)
+
+console.log("\n--END Q8--");
+// Q9)
+console.log("--Start Q9--\n");
+
+let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
+let dog1 = 'Bingo';
+let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+
+// let moreSports = teamSports;
+let moreSports = [...teamSports]
+moreSports.push('Football', 'Rugby')
+moreSports.unshift('Netball', 'Basketball')
+
+let dog2 = dog1
+dog2 = 'Spring'
+
+// let cat2 = cat1
+let cat2 = {...cat1}
+cat2.name = 'Gizmo'
+
+console.log('teamSports:', teamSports)
+console.log('moreSports:', moreSports)
+console.log('dog1:', dog1)
+console.log('dog2:', dog2)
+console.log('cat1:', cat1)
+console.log('cat2:', cat2)
+
+/*
+d)
+teamSports added values to the front and back of its list. 
+dog1 has not changed.
+cat1 name changed.
+teamSports and cat1 store complex data types which are stored by reference in memory.
+Using methods like .name, .push & .unshift change the information stored at the reference point.
+dog1 stores a primitive data type and holds a different reference to dog2 so changes made in one dont affect the other.
+*/
+
+
+
+console.log("\n--END Q9--");
+// Q10)
+console.log("--Start Q10--\n");
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.human = true;
+  this.canDrive = age >= 18;
+}
+
+let person1 = new Person('Joffrey Baratheon', 18)
+let person2 = new Person('Arya, Stark', 13)
+
+console.log(person1)
+console.log(person2)
+
+class PersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.human = true;
+    this.canDrive = age >= 18;
+  }
+}
+
+let person3 = new PersonClass('John Snow', 20)
+
+console.log(person3)
+
+console.log("\n--END Q10--");
